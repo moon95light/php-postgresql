40c929a1801d393bc4dfbd28fbd201062fe831f1
antonius95 <antonio.torres.tech@gmail.com>
2023-11-21 22:19:48 +0100

    feat: add the function ago

--- index.php
+++ index.php
@@ -71,3 +71,75 @@ SET info = info - '$userId'
 WHERE subscriptions.id = '$mycasdoorId';";
 
 $result = pg_query($db, $query);
+
+
+/**
+ * @description  : Created at 5 years ago
+ */
+
+ function timeAgo($time_ago)
+{
+    $time_ago = strtotime($time_ago);
+    $cur_time   = time();
+    $time_elapsed   = $cur_time - $time_ago;
+    $seconds    = $time_elapsed ;
+    $minutes    = round($time_elapsed / 60 );
+    $hours      = round($time_elapsed / 3600);
+    $days       = round($time_elapsed / 86400 );
+    $weeks      = round($time_elapsed / 604800);
+    $months     = round($time_elapsed / 2600640 );
+    $years      = round($time_elapsed / 31207680 );
+    // Seconds
+    if($seconds <= 60){
+        return "just now";
+    }
+    //Minutes
+    else if($minutes <=60){
+        if($minutes==1){
+            return "Created at 1 minute ago";
+        }
+        else{
+            return "Created at $minutes minutes ago";
+        }
+    }
+    //Hours
+    else if($hours <=24){
+        if($hours==1){
+            return "Created at an hour ago";
+        }else{
+            return "Created at $hours hrs ago";
+        }
+    }
+    //Days
+    else if($days <= 7){
+        if($days==1){
+            return "Created at yesterday";
+        }else{
+            return "Created at $days days ago";
+        }
+    }
+    //Weeks
+    else if($weeks <= 4.3){
+        if($weeks==1){
+            return "Created at a week ago";
+        }else{
+            return "Created at $weeks weeks ago";
+        }
+    }
+    //Months
+    else if($months <=12){
+        if($months==1){
+            return "Created at a month ago";
+        }else{
+            return "Created at $months months ago";
+        }
+    }
+    //Years
+    else{
+        if($years==1){
+            return "Created at 1 year ago";
+        }else{
+            return "Created at $years years ago";
+        }
+    }
+}
